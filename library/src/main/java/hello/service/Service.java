6fd1f873724bcb3ddd09ffddab4116107f5aedec
Steve Davis <deevodavis71@gmail.com>
2017-06-28 11:46:43 +0100

    First commit

---
+++ library/src/main/java/hello/service/Service.java
@@ -0,0 +1,20 @@
+package hello.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Service
+{
+
+	private final String message;
+
+	public Service (String message)
+	{
+		this.message = "YYYY" + message;
+	}
+
+	public String message ()
+	{
+		return this.message;
+	}
+}
